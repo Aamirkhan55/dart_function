aacce6ae38d4c45c7a745692d4b5e13f16ff315b
Amir55143 <aamirnoor55143@gmail.com>
2023-12-25 12:23:12 +0500

    ReturnType

--- bin/dart_function.dart
+++ bin/dart_function.dart
@@ -1,8 +1,19 @@
 void main () {
-  print(multipication(20));
+  //Int
+  int result = multipication(20);
+  print(result);
+
+//String
+String fullName = nameWithSurname('Aamir ');
+print(fullName);
+
 }
 
 // returntype funcName parameters 
 int multipication (int n) {
   return n * 10;
+}
+
+String nameWithSurname (String s) {
+  return "${s}Khan";
 }
